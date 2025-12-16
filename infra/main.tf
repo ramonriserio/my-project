fca819cd2a18e78f6f8e1fbc8649065bf2e22938
RamonRiserio <ramon.riserio@devops.eng>
2025-12-16 13:28:44 -0300

    Acrescentando a output public_ip

--- infra/main.tf
+++ infra/main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "ec2_sg" {
     }
 }
 
-resource "aws_instance" "state_ec2" {
+resource "aws_instance" "ec2" {
     ami = "ami-0ecb62995f68bb549"	# Ubuntu Server 24.04 LTS
     instance_type = "t2.micro"
 #    key_name = "humangov-ec2-key"	# com a role isso é mesmo necesário ???
@@ -71,4 +71,8 @@ resource "aws_instance" "state_ec2" {
     tags = {
         Name = var.instance_name
     }
+}
+
+output "public_ip" {
+  value       = aws_instance.ec2.public_ip
 }
