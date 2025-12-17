15d79fc4aae4420202f4f9ee54095ef1aa067519
RamonRiserio <ramon.riserio@devops.eng>
2025-12-16 22:15:36 -0300

    Tentando instalar o docker novamente

--- infra/main.tf
+++ infra/main.tf
@@ -58,12 +58,19 @@ resource "aws_security_group" "ec2_sg" {
     }
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
-    
+
+#    ingress {
+#        from_port = 0
+#        to_port = 0
+#        protocol = "-1"
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+
     tags = {
         Name = var.sg_name
     }
