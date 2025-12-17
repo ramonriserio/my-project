bde02022169d84f91429fd1170cb5f03b69c8cca
RamonRiserio <ramon.riserio@devops.eng>
2025-12-17 20:17:58 -0300

    Deixando habilitadas apenas as portas 22, 80 e 443

--- infra/main.tf
+++ infra/main.tf
@@ -44,9 +44,26 @@ resource "aws_security_group" "ec2_sg" {
     description = "Allow ingress traffic on ports 22 and 80"
 
     ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
