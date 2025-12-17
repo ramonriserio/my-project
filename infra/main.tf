4fcb8a958c41df41d174a0581d61c1e083a7f29e
RamonRiserio <ramon.riserio@devops.eng>
2025-12-16 22:22:03 -0300

    Tentando novamente

--- infra/main.tf
+++ infra/main.tf
@@ -64,12 +64,19 @@ resource "aws_security_group" "ec2_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
 
-#    ingress {
-#        from_port = 0
-#        to_port = 0
-#        protocol = "-1"
-#        cidr_blocks = ["0.0.0.0/0"]
-#    }
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
     tags = {
         Name = var.sg_name
