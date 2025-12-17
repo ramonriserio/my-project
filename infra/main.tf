b715775ccafe00ad54938851d395d2678483b2bd
RamonRiserio <ramon.riserio@devops.eng>
2025-12-16 22:26:51 -0300

    Tentando mais uma vez

--- infra/main.tf
+++ infra/main.tf
@@ -44,27 +44,6 @@ resource "aws_security_group" "ec2_sg" {
     description = "Allow ingress traffic on ports 22 and 80"
 
     ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
         from_port = 0
         to_port = 0
         protocol = "-1"
