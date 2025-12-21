691938a15b2b62510787052b80761c4423ed68ad
RamonRiserio <ramon.riserio@devops.eng>
2025-12-21 19:53:24 -0300

    Fazendo nova correção na role

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24" 
-  map_public_ip_on_launch = true          # Garante IP público para baixar Docker
+  map_public_ip_on_launch = true          # Garante IP público para baixar Docker 
   availability_zone       = "us-east-1a"  
 
   tags = {
